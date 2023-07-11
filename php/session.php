e97f36b78251f34ed17b05bf4dffac064cb0da35
Stephen Alonzo <stephena0228@gmail.com>
2023-07-11 14:10:28 -0700

    Updated session.php to check for user inactivity

--- php/session.php
+++ php/session.php
@@ -1,1 +1,18 @@
-<?php session_start(); ?>
+<?php 
+session_start();
+
+// set timeout period in seconds
+$inactive = 10;
+
+// check to see if $_SESSION['timeout'] is set
+if(isset($_SESSION['timeout']) ) {
+	$session_life = time() - $_SESSION['timeout'];
+	if($session_life > $inactive)
+        { session_destroy(); header("Location: login.php"); }
+}
+
+$_SESSION['timeout'] = time();
+
+// echo $inactive;
+
+?>
