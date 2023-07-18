7e08684e9020532c02d69f36de896e58273c271f
Stephen Alonzo <stephena0228@gmail.com>
2023-07-17 21:44:15 -0700

    Consolidated timesheet.php elements within index

--- index.php
+++ index.php
@@ -19,21 +19,87 @@ require_once ('controller.php');
 
 <body>
     <section class="px-4 py-6">
-        <div class="container mx-auto space-y-4">
+        <div class="container mx-auto">
             <div class="flex flex-col items-center justify-center space-y-1">
                 <div id="time" class="font-semibold text-3xl"></div>
                 <div id="date"></div>
             </div>
-            <div class="flex items-center justify-center space-x-4">
-                <form action="" method="post">
-                    <?php 
-                    
-                    $params = buttonGroup($params);
-
-                    echo $params['buttonGroup'];
-                    
-                    ?>
+        </div>
+    </section>
+    <section class="px-4 py-6">
+        <div class="container mx-auto w-2/3 space-y-4">
+            <div class="flex flex-row items-center justify-between">
+                <form action="" method="post" class="w-1/3 flex flex-row items-end space-x-4">
+                    <div>
+                        <label for="HeadlineAct" class="block text-sm font-medium text-gray-900">
+                            Select Pay-Period:
+                        </label>
+                        <select name="pay_period" id="HeadlineAct" class="mt-1.5 p-2 w-full rounded-lg border border-gray-300 text-gray-700 sm:text-sm">
+                            <?php
+                            $params = viewPayPeriods($params);
+                            ?>
+                        </select>
+                    </div>
+                    <button type="submit" name="pp_view" class="rounded-lg bg-blue-600 px-5 py-2 font-medium text-white">View</button>
                 </form>
+                <div class="flex items-center justify-center space-x-4">
+                    <form action="" method="post" class="m-0">
+                        <?php 
+                    
+                        $params = buttonGroup($params);
+                    
+                        ?>
+                    </form>
+                </div>
+            </div>
+            <div class="rounded-lg border border-gray-200 overflow-hidden">
+                <table class="w-full divide-y-2 divide-gray-200 bg-white text-sm">
+                    <thead>
+                        <tr>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                Date
+                            </th>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                In Day
+                            </th>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                Out Break
+                            </th>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                In Break
+                            </th>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                Out Lunch
+                            </th>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                In Lunch
+                            </th>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                Out Break
+                            </th>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                In Break
+                            </th>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                Out Day
+                            </th>
+                            <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                                Total Hours
+                            </th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="divide-y divide-gray-200">
+                        <?php
+
+                        $params = readOnlyTimesheet($params);
+
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+            <div class="flex flex-row items-center justify-end">
+                <span class="font-medium">Total Hours worked: <?php $params = viewTotalHours($params); echo $params['totalHours']; ?></span>
             </div>
         </div>
     </section>
